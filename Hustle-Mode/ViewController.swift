3a606c608ce806322bf20aba558510a9e509915f
Mirza Iqbal <jahanzaibhere@gmail.com>
2019-08-12 21:39:19 +0200

    Initial Commit

---
+++ Hustle-Mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hustle-Mode
+//
+//  Created by Mirza Iqbal on 12.08.19.
+//  Copyright © 2019 Mirza Iqbal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
